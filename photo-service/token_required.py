8f0d03beb4f3fc68fa1d0bc89f6e7e3c7254a0d1
Nathan Callaghan <49479587+Nathancal@users.noreply.github.com>
2022-01-03 19:58:59 +0000

    photo-service-update

---
+++ photo-service/token_required.py
@@ -0,0 +1,56 @@
+from functools import wraps
+import logging
+import re
+import azure.functions as httpReqFunc
+import jwt
+import json
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+credential = DefaultAzureCredential()
+vaultUri = f"https://keyconfig.vault.azure.net"
+keyVault = SecretClient(vault_url=vaultUri, credential=credential)
+
+
+def jwt_required(func):
+    @wraps(func)
+    def jwt_required_wrapper(req: httpReqFunc.HttpRequest, *args, **kwargs):
+       
+        logging.info(req.headers.get("Host"))
+        logging.info(req.headers.get("x-access-token"))
+
+        token = None
+
+        if 'x-access-token' in req.headers:
+            token = req.headers['x-access-token']
+
+        if not token:
+
+            resObj = {"message": "Token is missing"}
+
+            return httpReqFunc.HttpResponse(json.dumps(resObj),status_code=401)
+            
+     
+        secret_name= "token-secret"
+        tokenSecret = keyVault.get_secret(secret_name)
+
+        logging.info(tokenSecret.value)
+        logging.info(token)
+
+        try:
+
+            secret_name= "token-secret"
+            tokenSecret = keyVault.get_secret(secret_name)
+
+            data = jwt.decode(str(token), tokenSecret.value, algorithms="HS256")
+
+        except:
+            
+            resObj = {"message": "Token is invalid"}
+
+            return httpReqFunc.HttpResponse(json.dumps(resObj), status_code=401)
+
+
+        return func(req, *args, **kwargs)
+
+    return jwt_required_wrapper
